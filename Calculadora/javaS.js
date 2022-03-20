442da15e35a43605db7115d9fafe1878932cebe4
Edward Laiton <edwlz@hotmail.com>
2022-03-19 21:54:35 -0500

    Creando repositorio para el taller entreglable numero 2

---
+++ Calculadora/javaS.js
@@ -0,0 +1,112 @@
+    var panta = document.getElementById("pantalla");
+    var nUno = document.getElementById("uno");
+    var nDos = document.getElementById("dos");
+    var nTres = document.getElementById("tres");
+    var nCuatro = document.getElementById("cuatro");
+    var nCinco = document.getElementById("cinco");
+    var nSeis = document.getElementById("seis");
+    var nSiete = document.getElementById("siete");
+    var nOcho = document.getElementById("ocho");
+    var nNueve = document.getElementById("nueve");
+    var nCero = document.getElementById("cero");
+    var nSuma = document.getElementById("btnSuma");
+    var nResta = document.getElementById("btnResta");
+    var nMultiplica = document.getElementById("btnMultiplica");
+    var nDivide = document.getElementById("btnDivide");
+    var nLimpia = document.getElementById("btnLimpia");
+    var nIgual = document.getElementById("btnIgual");
+    var resutadoFinal = document.getElementById("h4Resultado");
+    var num1 = 0;
+    var num2 = 0;
+    var resultado = 0;
+    var operadorX;
+    
+nUno.addEventListener("click", function () {
+    panta.value = panta.value + "1";
+    var uno1 = parseInt(panta.value);
+});
+nDos.addEventListener("click", function () {
+    panta.value = panta.value + "2";
+    var dos2 = parseInt(panta.value);
+});
+nTres.addEventListener("click", function () {
+    panta.value = panta.value + "3";
+    var tres3 = parseInt(panta.value);
+});
+nCuatro.addEventListener("click", function () {
+    panta.value = panta.value + "4";
+    var cuatro4 = parseInt(panta.value);
+});
+nCinco.addEventListener("click", function () {
+    panta.value = panta.value + "5";
+    var cinco5 = parseInt(panta.value);
+});
+nSeis.addEventListener("click", function () {
+    panta.value = panta.value + "6";
+    var seis6 = parseInt(panta.value);
+});
+nSiete.addEventListener("click", function () {
+    panta.value = panta.value + "7";
+    var siete7 = parseInt(panta.value);
+});
+nOcho.addEventListener("click", function () {
+    panta.value = panta.value + "8";
+    var ocho8 = parseInt(panta.value);
+});
+nNueve.addEventListener("click", function () {
+    panta.value = panta.value + "9";
+    var nueve9 = parseInt(panta.value);
+});
+nCero.addEventListener("click", function () {
+    panta.value = panta.value + "0";
+    var cero0 = parseInt(panta.value);
+});
+nLimpia.addEventListener("click", function(){
+    limpiar();
+});
+nSuma.addEventListener("click", function(){
+    operadorX = "+";
+    num1 = parseInt(panta.value);
+    limpiar();
+});
+nResta.addEventListener("click", function(){
+    operadorX = "-";
+    num1 = parseInt(panta.value);
+    limpiar();
+});
+nMultiplica.addEventListener("click", function(){
+    operadorX = "*"
+    num1 = parseInt(panta.value);
+    limpiar();
+});
+nDivide.addEventListener("click", function(){
+    operadorX = "/";
+    num1 = parseInt(panta.value);
+    if (num1 == 0){
+        alert("Su numero no puede ser 0")
+    }
+    limpiar();
+});
+nIgual.addEventListener("click", function(){
+    num2 = parseInt(panta.value);
+    operadores(operadorX);
+    if ((operadores(operadorX)) == "/" && num2 == 0){
+        alert("Su numero no puede dividirse en 0")
+        limpiar();
+    }
+});
+function limpiar(){
+    panta.value = "";
+    resutadoFinal.value = "";
+}
+function operadores (operar){
+    if (operar == "+"){
+        resultado = num1 + num2;
+    }else if (operar == "-"){
+        resultado = num1 - num2;
+    }else if (operar == "*"){
+            resultado = num1 * num2;
+    }else if (operar == "/"){
+        resultado = num1/num2;
+    }resutadoFinal.value = resultado;
+}
